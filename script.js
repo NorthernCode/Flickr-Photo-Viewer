456e696153deabc80c981f4b3b2da291e60afe65
unknown <antti.o.virtanen@aalto.fi>
2015-12-26 15:11:25 +0200

    better ajax/json implementation

--- script.js
+++ script.js
@@ -11,22 +11,27 @@ function fvInit(){
 }
 
 function getPhotoIDs(albumId){
-	var scriptTag = document.createElement('SCRIPT');
-	scriptTag.src = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&format=json&api_key=81bcafb1419ee85e54f9c29392eb356b&photoset_id=' + albumId;
-	 
-	document.getElementsByTagName('HEAD')[0].appendChild(scriptTag);
+	var url = 'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&format=json&nojsoncallback=1&api_key=81bcafb1419ee85e54f9c29392eb356b&photoset_id=' + albumId; 
+	xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+	    if (xhttp.readyState == 4 && xhttp.status == 200) {
+	     	getPhotoUrls(JSON.parse(xhttp.responseText));
+	    }
+	};
+	xhttp.open("GET", url, true);
+	xhttp.send();
 }
 
-function jsonFlickrApi(data){
+function getPhotoUrls(data){
 	var xhttp = [];
 	for (var i = 0; i < data.photoset.photo.length; i++){
 		(function (i){
-			element.innerHTML += '<div id="fv-photo' + i + '"><p>' + data.photoset.photo[i].title+ '</p></div>';
-			var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&format=json&api_key=81bcafb1419ee85e54f9c29392eb356b&photo_id=' + data.photoset.photo[i].id; 
+			element.innerHTML += '<div id="fv-photo-' + i + '"><p>' + data.photoset.photo[i].title+ '</p></div>';
+			var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&format=json&nojsoncallback=1&api_key=81bcafb1419ee85e54f9c29392eb356b&photo_id=' + data.photoset.photo[i].id; 
 			xhttp[i] = new XMLHttpRequest();
 			xhttp[i].onreadystatechange = function() {
 			    if (xhttp[i].readyState == 4 && xhttp[i].status == 200) {
-			     	setPhoto(JSON.parse(xhttp[i].responseText.split('(')[1].split(')')[0]), i);
+			    	setPhoto(JSON.parse(xhttp[i].responseText), i);
 			    }
 			};
 			xhttp[i].open("GET", url, true);
@@ -38,7 +43,7 @@ function jsonFlickrApi(data){
 function setPhoto(photoSizes, index){
 	for (var i = 0; i < photoSizes.sizes.size.length; i++){
 		if (photoSizes.sizes.size[i].height >= wantedSize) {
-			document.getElementById('fv-photo' + index).innerHTML += '<img src="' + photoSizes.sizes.size[i].source + '"/>';
+			document.getElementById('fv-photo-' + index).innerHTML += '<img src="' + photoSizes.sizes.size[i].source + '"/>';
 			return;
 		}
 	}
